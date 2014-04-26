3849dc6029cf8d51b524b3d094c54b755f73692d
jpmoresmau <jp@moresmau.fr>
2014-04-26 17:39:29 +0200

    countryList, ready to be used in a yesod field

--- src/Data/CountryCodes.hs
+++ src/Data/CountryCodes.hs
@@ -1,6 +1,7 @@
 module Data.CountryCodes  (
     CountryCode(..)
   , allNames
+  , countryList
   , fromMText
   , fromText
   , fromMName
@@ -13,7 +14,13 @@ import Control.Arrow ((&&&))
 import Data.Text (Text)
 
 import Data.CountryCodes.ISO31661
+import Data.List (sortBy)
+import Data.Function (on)
 
 -- | list all codes with names
 allNames :: [(CountryCode,Text)]
 allNames = map (id &&& toName) $ enumFrom minBound
+
+-- | list of names sorted by alphabetical order, with country code
+countryList :: [(Text,CountryCode)]
+countryList = sortBy (compare `on` fst) $ map (toName &&& id) $ enumFrom minBound
